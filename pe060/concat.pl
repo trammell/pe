34af919e480e3feee99c3dbff83c1a2248582779
John Trammell <trammell@voh.localdomain>
2008-12-25 13:13:30 -0600

    new file to investigate prime concatenation

---
+++ pe060/concat.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -l
+
+use strict;
+use warnings FATAL => 'all';
+use lib "$ENV{HOME}/work/project-euler/lib";
+use Primes;
+
+my @families;
+
+foreach my $prime (primes()) {
+
+
+}
+
+
+
+unless (caller()) {
+
+
+}
+
+1;
+
+__END__
+
+
