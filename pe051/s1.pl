39f3b4f7a09fd538726ae8f5aa9b16a9388080f8
John Trammell <trammell@voh.localdomain>
2008-12-25 11:33:37 -0600

    finished

--- pe051/s1.pl
+++ pe051/s1.pl
@@ -5,10 +5,38 @@
 use strict;
 use warnings FATAL => 'all';
 use lib "$ENV{HOME}/work/project-euler/lib";
-use Data::Dumper;
-use Math::Combinatorics;
-use Sieve;
+use Primes;
 
-my @primes = grep { length($_) == 6 } Sieve->primes();
-print "nprimes=", scalar @primes;
+for my $n (0 .. 999) {
+    print "n=$n" if $n % 50 == 0;
+    next unless $n =~ /[1379]$/;
+    my $s = sprintf '%03d', $n;
+    for my $i (1 .. 3) {
+        my @v = vary($s,$i);
+        for my $v (@v) {
+            my @p =
+                grep { is_prime($_) }
+                map {
+                    (my $tmp = $v) =~ s/X/$_/g;
+                    $tmp;
+                } 0 .. 9;
+            next unless @p >= 7;
+            print "$v => @p";
+        }
+    }
+}
+
+sub vary {
+    my ($s, $n) = @_;
+    return ($s) if $n == 0;
+    my %out;
+    for my $v (vary($s,$n-1)) {
+        map {
+            my $tmp = $v;
+            substr($tmp,$_,0,'X');
+            $out{ $tmp } = 1;
+        } 0 .. length($v)-1;
+    }
+    return sort keys %out;
+}
 
