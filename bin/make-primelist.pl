c6eb4313684031080d797a8be66d2f6f1ea2ad47
John Trammell <trammell@voh.localdomain>
2008-12-26 12:43:12 -0600

    use Sieve to precalculate a list of primes in a Perl module

---
+++ bin/make-primelist.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -l
+
+use strict;
+use warnings FATAL => 'all';
+use lib "../lib/";
+use Sieve max => 10_000_000;
+use Text::Wrap;
+
+$Text::Wrap::columns = 70;
+
+my $sep = q( ) x 4;
+
+print <<"__pkg__";
+package Primes;
+
+use strict;
+use warnings FATAL => 'all';
+
+my \@PRIMES = (
+@{[ Text::Wrap::wrap($sep,$sep,join q(, ), Sieve->primes()) ]}
+);
+
+unless (caller()) {
+    print join(q(, ), \@PRIMES[0 .. 9]), "... \$PRIMES[-1]\n";
+}
+
+sub primes {
+    return \@PRIMES;
+}
+
+1;
+__pkg__
+
