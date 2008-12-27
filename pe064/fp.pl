357134d889e6b61057a2bf989678608214837ef1
John Trammell <trammell@voh.localdomain>
2008-12-27 16:03:28 -0600

    trying to speed this mofo up

--- pe064/fp.pl
+++ pe064/fp.pl
@@ -15,7 +15,7 @@ my $odd = 0;
 for my $n (1 .. 10_000) {
     warn "n=$n odd=$odd\n" if $n % 100 == 0;
     if (my $sqrt = $is_square{$n}) {
-        warn "skipping perfect square: sqrt($n)=$sqrt\n";
+        warn "skipping perfect square: $sqrt^2=$n\n";
         next;
     }
     $odd++ if find_period($n) % 2 == 1;
@@ -23,16 +23,37 @@ for my $n (1 .. 10_000) {
 
 print "count of numbers with odd period: $odd";
 
+my %per;
+
 sub find_period {
-    my $s = sqrt($_[0]);
-    my $a0 = int($s);
-    $s = 1.0 / ($s - $a0);
-    my $p = 0;
+    my $r = sqrt($_[0]);
+    my @terms;
+    my %seen;
     while (1) {
-        $p++;
-        last if int($s) == 2 * $a0;
-        $s = 1.0 / ($s - int($s));
+        my $t = next_term($r);
+        return $per{ $t->[0] } if $per{ $t->[0] };
+        push @terms, $t;
+        last if $seen{ $t->[0] };
+        $seen{ $t->[0] } = 1;
+        $r = $t->[2];
     }
+    my $s = pop(@terms)->[0];
+    my $p = 0;
+    for (reverse @terms) {
+        $p++;
+        last if $_->[0] eq $s;
+    }
+    $per{ $_->[0] } = $p for @terms;
     return $p;
 }
 
+my %nt;
+sub next_term {
+    my $x = shift;
+    my $s = sprintf '%.9f', $x;
+    $nt{$s} ||= do {
+        my $a = int($x);
+        my $f = 1.0 / ($x - $a);
+        [ $s, $a, $f ];
+    };
+}
