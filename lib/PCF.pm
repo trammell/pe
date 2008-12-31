5968ceb32e6a447bd3a31769591e470a6448ba81
John Trammell <johntrammell@gmail.com>
2008-12-31 14:16:58 -0600

    got a fast prime counting function going

--- lib/PCF.pm
+++ lib/PCF.pm
@@ -8,15 +8,31 @@ use Data::Dumper;
 use Primes;
 
 unless (caller()) {
-    for my $i (1 .. 20) {
-        print "pcf($i)=", pcf($i);
+    my @correct = (0,0,1,2,2,3,3,4,4,4,4,5,5,6,6,6,6,7,7,8,8,8,8);
+    for my $i (0 .. $#correct) {
+        my $c = $correct[$i];
+        my $pcf = pcf($i);
+        if ($c == $pcf) {
+            warn "pcf($i)=$pcf\n";
+        }
+        else {
+            warn "got pcf($i)=$pcf; should be $c\n";
+        }
     }
 }
 
+my @PCF;
+
 sub pcf {
-
-
-
+    my $n = shift;
+    unless (@PCF) {
+        @PCF = (0, 0, 1, 2, 2);
+    }
+    while ($n > $#PCF) {
+        my $i = $#PCF;
+        push @PCF, $PCF[$i] + (is_prime($i + 1) ? 1 : 0);
+    }
+    return $PCF[$n];
 }
 
 1;
@@ -25,6 +41,9 @@ sub pcf {
 
 The prime counting function is the function pi(x) giving the number of
 primes less than or equal to a given number x.
+
+http://mathworld.wolfram.com/PrimeCountingFunction.html
+http://www.research.att.com/~njas/sequences/A000720
 
 =cut
 
