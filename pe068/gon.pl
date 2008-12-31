62242ea4bbbae6eb1f4fbf3b57467fd829f82475
John Trammell <trammell@voh.localdomain>
2008-12-30 19:58:04 -0600

    cleaned up is_magic

--- pe068/gon.pl
+++ pe068/gon.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings FATAL => 'all';
+use List::Util 'sum';
 use List::MoreUtils 'all';
 
 print is_magic([4,6,5,3,2,1]);
@@ -12,8 +13,8 @@ sub is_magic {
     my $n = scalar(@$gon) / 2;
     my @sum;
     for my $i (0 .. $n - 1) {
-        $sum[$i] = $gon->[$i] + $gon->[$n + ($i % $n)] +
-            $gon->[$n + (($i + 1) % $n)];
+        my @j = ($i, $n + ($i % $n), $n + (($i + 1) % $n));
+        $sum[$i] = sum map { $gon->[$_] } @j;
     }
     my $s = shift @sum;
     return all { $s == $_ } @sum;
