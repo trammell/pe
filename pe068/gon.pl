3a513cddb6558754aa8fa4fd1a1be54896202bd0
John Trammell <johntrammell@gmail.com>
2008-12-31 12:40:59 -0600

    trying to understand the problem

--- pe068/gon.pl
+++ pe068/gon.pl
@@ -2,9 +2,17 @@
 
 use strict;
 use warnings FATAL => 'all';
+use Math::Combinatorics;
 use Gon;
 
+my $c = Math::Combinatorics->new(count => 9, data => [ 1 .. 9 ]);
+#my $c = Math::Combinatorics->new(count => 6, data => [ 1 .. 6 ]);
 
-
-
+my $count = 0;
+while (my @p = $c->next_permutation) {
+    my @a = (10, @p[ 0 .. 3 ] );
+    my @b = (@p[ 4 .. 8 ] );
+    print "@a | @b";
+    last if $count++ > 25;
+}
 
