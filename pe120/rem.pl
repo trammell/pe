9c8ecb6cd9551ecfa8d570e2394ad6ac6e107665
John Trammell <johntrammell@gmail.com>
2009-01-02 12:37:07 -0600

    using modular exponentiation, but it's still slooooooow...

--- pe120/rem.pl
+++ pe120/rem.pl
@@ -3,31 +3,34 @@
 use strict;
 use warnings FATAL => 'all';
 
-
 my $sum = 0;
 
 for my $a (3 .. 1000) {
     my $mr = maxrem($a);
     $sum += $mr;
-    if ($a <= 20) {
-        print "maxrem($a)=$mr sum=$sum";
-    }
-    last if $a > 10;
 }
 
 print $sum;
 
 sub maxrem {
     my $a = shift;
+    my $a2 = $a * $a;
     my $rmax = 0;
     my $nmax = 0;
-    for my $n (3 .. $a * $a) {
-        my $r =((($a - 1) ** $n) + (($a + 1) ** $n)) % ($a * $a);
+    my $T1 = my $t1 = $a - 1;
+    my $T2 = my $t2 = $a + 1;
+    my $n = 0;
+    while (1) {
+        $n++;
+        $t1 = ($t1 + $a - 1) % $a2;
+        $t2 = ($t2 + $a + 1) % $a2;
+        last if $T1 == $t1 && $T2 == $t2;
+        my $r = ($t1 + $t2) % $a2;
         next unless $r > $rmax;
         $rmax = $r;
         $nmax = $n;
     }
-    print "   r_max($a)=$rmax at n=$nmax";
+    print "r_max(a=$a;n=$nmax)=$rmax";
     return $rmax;
 }
 
