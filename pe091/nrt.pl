a9fe509e462b7877dc587064e29ea8b5a97da3e7
John Trammell <johntrammell@gmail.com>
2009-01-05 10:14:03 -0600

    added perp check; need to check initial conditions

--- pe091/nrt.pl
+++ pe091/nrt.pl
@@ -8,7 +8,9 @@ my @origin = (0,0);
 my $DEBUG = 1;
 
 for my $d (0 .. 3) {
-    print "nrt_edge($d)=", nrt_edge($d);
+    print "nrt($d)=", nrt($d);
+    print "nrt_interior($d)=", nrt_interior($d);
+    print "nrt_boundary($d)=", nrt_boundary($d);
 }
 
 =head2 nrt($d)
@@ -17,18 +19,46 @@ Returns the number of triangles OPQ
 
 =cut
 
+my $NRT;
+
 sub nrt {
     my $d = shift;
-
-
-
-
-
+    return 0 if $d < 1;
+    unless (defined $NRT->[$d]) {
+        $NRT->[$d] = nrt($d - 1) + nrt_interior($d) + nrt_boundary($d);
+    }
+    return $NRT->[$d];
 }
 
 sub nrt_interior {
+    my $d = shift;
 
+    my @edge = map [ $_, $d ], 0 .. $d;
 
+    my $count = 0;
+
+    for my $x (0 .. $d - 1) {
+        for my $y (0 .. $d - 1) {
+            next unless $x && $y;   # skip (0,0)
+            my $vi = [$x,$y];       # vector to interior
+            for my $ve (@edge) {    # vector to edge
+                my $v = [
+                    $vi->[0] - $ve->[0],
+                    $vi->[1] - $ve->[1],
+                ];
+                if (perp($v,$ve)) {
+                    $count += 2;
+                    next;
+                }
+                elsif (perp($v,$vi)) {
+                    $count += 2;
+                    next;
+                }
+            }
+        }
+    }
+
+    return $count;
 }
 
 sub nrt_boundary {
@@ -37,6 +67,9 @@ sub nrt_boundary {
     return 3 * (2 * $d - 1);
 }
 
-
-
+sub perp {
+    my ($v,$w) = @_;
+    my $dot = $v->[0] * $w->[0] + $v->[1] * $w->[1];
+    return (abs($dot) < 1e-9) ? 1 : 0;
+}
 
