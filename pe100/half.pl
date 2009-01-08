ee1cf3364e202576a4e5d2af1390a10d2c632cd2
John Trammell <johnt@localhost.localdomain>
2009-01-08 15:12:02 -0600

    hmm, 1e12?

--- pe100/half.pl
+++ pe100/half.pl
@@ -2,23 +2,49 @@
 
 use strict;
 use warnings FATAL => 'all';
+use lib "$ENV{HOME}/work/project-euler/lib";
+use GCD 'gcd';
 
-my $r2 = sqrt(2) / 2;
+my $r2 = 0.5 * sqrt(2);
 
 print "r2=$r2";
 
-for (my $t = 2; $t < 50; $t++) {
-    my $b = 1 + int($t * $r2);
-    my $f = $t * ($t - 1) / ( $b * ($b - 1));
-    print "$t, $b, $f";
+for (my $t = 1_000_000_000; $t < 50000; $t++) {
+
+    my $b   = 1 + int($t * $r2);
+    my @num = ($b, $b - 1);
+    my @den = ($t, $t - 1);
+
+    if ((my $gcd = gcd($num[0],$den[1])) > 1) {
+        $num[0] /= $gcd;
+        $den[1] /= $gcd;
+    }
+
+    if ((my $gcd = gcd($num[0],$den[0])) > 1) {
+        $num[0] /= $gcd;
+        $den[0] /= $gcd;
+    }
+
+    if ((my $gcd = gcd($num[1],$den[0])) > 1) {
+        $num[1] /= $gcd;
+        $den[0] /= $gcd;
+    }
+
+    if ((my $gcd = gcd($num[1],$den[1])) > 1) {
+        $num[1] /= $gcd;
+        $den[1] /= $gcd;
+    }
+
+    my $n = $num[0] * $num[1];
+    my $d = $den[0] * $den[1];
+
+    if ((my $gcd = gcd($n,$d) > 1)) {
+        $n /= $gcd;
+        $d /= $gcd;
+    }
+
+    next unless $n == 1 and $d == 2;
+
+    print "$t $n $d";
 }
-
-
-
-for my $b (1 .. 1000) {
-    my $m = 8 * $b * ($b - 1);
-    next unless (8 * $b * ($b-1)) % 4 == 3;
-    print $b;
-}
-
 
