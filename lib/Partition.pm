35b58482fa8b93297bdd920127925a7f6a2c581b
John Trammell <johntrammell@gmail.com>
2009-04-15 13:16:24 -0500

    fixed recursion problem, but it's still slow.  Can I speed this up?

--- lib/Partition.pm
+++ lib/Partition.pm
@@ -38,7 +38,7 @@ my $Pt;
 my $TRUNC = 1_000_000;
 
 sub Pt {
-    my ($k,$n) = @_;
+    my ($k,$n,$skip_setup) = @_;
 
     # boundary conditions
     return 0 if $n < 0;
@@ -46,31 +46,33 @@ sub Pt {
     return 0 if $k > $n;
     return 1 if $k == $n;
 
-    # preemptively calculate Pt($i,$n) for $k <= $i <= $n + 1
-    my $lattice = sub { 5 * (1 + int($_[0]/5)) };
-    unless (exists $Pt->{ $lattice->($k) }{ $lattice->($n)} ) {
-        init($n);
-    }
-
     unless (exists $Pt->{$k}{$n}) {
-        if (($k % 10 == 0) && ($n % 10 == 0)) {
+
+        unless ($skip_setup) {
+            # preemptively calculate the necessary predecessors to calculate Pt($k,$n)
+            # for $k <= $i <= $n + 1
+            setup($n);
+        }
+
+        if (($k % 100 == 0) && ($n % 100 == 0)) {
             print "calculating Pt(k=$k,n=$n) ...";
         }
-        my $p1 = Pt($k + 1, $n);
-        my $p2 = Pt($k, $n - $k);
+        my $p1 = Pt($k + 1, $n, $skip_setup);
+        my $p2 = Pt($k, $n - $k, $skip_setup);
         $Pt->{$k}{$n} = ($p1 + $p2) % $TRUNC;
     }
     return $Pt->{$k}{$n};
 }
 
-sub init {
+sub setup {
     my $max = shift;
-    my $incr = 5;
-    for (my $n = $incr; $n <= $max; $n += $incr) {
-        for (my $k = $incr; $k <= $n; $k += $incr) {
+    for (my $n = 1; $n <= $max; $n++) {
+        for my $k (reverse(1 .. $n)) {
             next if exists $Pt->{$k}{$n};
-            print "initializing Pt($k,$n)...";
-            Pt($k,$n);
+            if (($k % 100 == 0) && ($n % 100 == 0)) {
+                print "initializing Pt($k,$n)...";
+            }
+            Pt($k,$n,1);
         }
     }
 }
--- pe078/coins.pl
+++ pe078/coins.pl
@@ -1,13 +1,10 @@
 #!/usr/bin/perl -l
-# vim: set ai et ts=4 tw=75 :
 
 use strict;
 use warnings FATAL => 'all';
 use lib '../lib';
 
 use Partition 'Pt';
-
-Partition::init(100);
 
 my @n = (
     4, 24, 99, 599, 2474, 14974, 14974, 30599, 46224, 61849, 77474, 93099
@@ -16,7 +13,7 @@ my @n = (
 for my $n (@n) {
     print "n=$n";
     my $pt = Pt(1,$n);
-    print "    Pt(1,$n)=$pt";
-   # last if $pt % 1_000_000 == 0;
+    print "  Pt(1,$n)=$pt";
+    last if $pt % 1_000_000 == 0;
 }
 
