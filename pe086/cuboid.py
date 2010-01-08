cd03643cbf4aa7f77521f2851b2352c1b944066f
John Trammell <johntrammell@gmail.com>
2010-01-08 15:44:17 -0600

    starting to play around with ideas

---
+++ pe086/cuboid.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.4
+
+"""
+
+"""
+
+m = 100
+
+def gcd(a,b):
+    if b: return gcd(b,a % b)
+    else: return a
+
+def are_legs(a,b):
+    # exactly one of a,b is odd
+    if (a + b) % 2 == 0:
+        return False
+
+
+def check_heights(a,b,m):
+    for h in range(1,q):
+                (p+q,r)
+                (p,q+r)
+                (p+r,q)
+
+
+
+for m in range(1,100):
+    for p in range(1,m+1):
+        for q in range(1,p+1):
+
+
+
+
+
+
+
+
