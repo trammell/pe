0f74a6a008db88be80323eff702294803b16301e
John Trammell <johntrammell@gmail.com>
2010-01-08 16:17:57 -0600

    added some more analysis

--- pe086/cuboid.py
+++ pe086/cuboid.py
@@ -17,6 +17,24 @@ def f(m):
     n = 0
     for p in range(1,m+1):
         for q in range(1,p+1):
+            pq = p * q
+            pm = p * m
+            qm = q * m
+            if q < m and p < m:     # thus pq < pm, qm
+
+
+
+            elif
+
+
+
+
+            # need to find the smallest of
+            #   (p+q)^2 + m^2 = p^2 + 2pq + q^2 + m^2
+            #   (p+m)^2 + q^2 = p^2 + 2pm + q^2 + m^2
+            #   (q+m)^2 + p^2 = p^2 + 2qm + q^2 + m^2
+            # equivalent to smallest of pq, pm, qm
+
             if find_triple(q,p,m):
                 n += 1
     return 3
