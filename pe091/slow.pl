e23cfb91e26e6efd6841191125e0ad2281d121c6
John Trammell <johntrammell@gmail.com>
2010-03-26 12:25:18 -0500

    added doco

--- pe091/slow.pl
+++ pe091/slow.pl
@@ -7,7 +7,7 @@ use Data::Dumper;
 my @origin = (0,0);
 my $DEBUG = 1;
 
-for my $d (1 .. 5) {
+for my $d (1 .. 10) {
     print "nrt($d)=", nrt($d);
 }
 
@@ -24,24 +24,37 @@ sub nrt {
 
     my $count = 0;
 
+    # loop over points (u,v) and (x,y), looking for right triangles
+
     for my $u (0 .. $d) {
         for my $v (0 .. $d) {
+
+            # skip origin
             next unless $u || $v;
+
             for my $x (0 .. $d) {
                 for my $y (0 .. $d) {
+
+                    # skip origin
                     next unless $x || $y;
+
+                    # skip if (u,v) == (x,y)
                     next if ($u == $x) && ($v == $y);
 
-                    my @s = (
-                        [ $u, $v ],
-                        [ $x, $y ],
-                        [ $u - $x, $v - $y ],
+                    # list of three vectors representing the sides of our
+                    # triangle
+                    my @vec = (
+                        [ $u, $v ],             # origin => (u,v)
+                        [ $x, $y ],             # origin => (x,y)
+                        [ $u - $x, $v - $y ],   # (x,y) => (u,v)
                     );
 
+                    # try all combinations of vectors looking for
+                    # perpendiculars
                     for ([0,1],[1,2],[0,2]) {
-                        my ($s1, $s2) = @s[ @$_ ];
-                        if (perp($s1,$s2)) {
-                            print "d=$d found (0,0) ($u,$v) ($x,$y)";
+                        my ($v1, $v2) = @vec[ @$_ ];
+                        if (perp($v1,$v2)) {
+                            #print "d=$d found (0,0) ($u,$v) ($x,$y)";
                             $count += 0.5;
                             last;
                         }
@@ -56,9 +69,9 @@ sub nrt {
 
 }
 
-# returns true if vectors (u,v) are perpendicular
+# calculate dot product; if vectors (v1,v2) are perpendicular the dot product
+# is zero.  Return true if vectors (u,v) are perpendicular.
 sub perp {
-    my ($u,$v) = @_;
-    return (abs($u->[0] * $v->[0] + $u->[1] * $v->[1]) < 1e-9) ? 1 : 0;
+    return abs($_[0][0] * $_[1][0] + $_[0][1] * $_[1][1]) < 1e-9;
 }
 
