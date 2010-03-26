7aa004a914ac2c909f0d1a6d1aa68b413664997d
John Trammell <johntrammell@gmail.com>
2010-03-26 12:25:44 -0500

    needs tests

--- pe091/NRT.pm
+++ pe091/NRT.pm
@@ -33,22 +33,33 @@ sub nrt {
     my $d = shift;
     $NRT ||= [0,3];
     unless (defined $NRT->[$d]) {
-        $NRT->[$d] = nrt_interior($d) + nrt_edge($d);
+        $NRT->[$d] = nrt_edge($d) + nrt_interior($d);
     }
     return $NRT->[$d];
 }
 
+# return the number of triangles with one point on the origin and the other
+# two points both on the "edge" (X=0, X=a, Y=0, Y=a).
+#   a=1 => 3
+#   a=2 => 9
+#   a=3 => 15
+sub nrt_edge {
+    my $a = shift;
+    return 6 * $a - 3;
+}
+
+# Return the number of triangles witn one point on the origin, one point on
+# the "edge", and one point in the interior of the box.
 sub nrt_interior {
     my $d = shift;
     my @ep = edge_points($d);
     my $count = 0;
 
     for my $x (0 .. $d - 1) {
-        for my $y (0 .. $d - 1) {
+        for my $y ($x .. $d - 1) {
             next unless $x && $y;   # skip (0,0)
             for my $e (@ep) {
-                next unless is_rt([$x,$y],$e);
-                $count++;
+                $count += 2 if is_rt([$x,$y],$e);
             }
         }
     }
@@ -56,16 +67,7 @@ sub nrt_interior {
     return $count;
 }
 
-# return the number of triangles with two points on the edge
-#   d=1 => 3
-#   d=2 => 9
-sub nrt_edge {
-    my $d = shift;
-    return 3 + (2 * $d - 1) + (2 * $d - 1); # XXX wtf
-    return 2 * $d + 1;
-}
-
-# list all points on the boundary (x == d || y == d)
+# list all points on the outer edge of the box (i.e. the lines X=d, Y=d)
 sub edge_points {
     my $d = shift;
     my @edge = map [ $_, $d ], 0 .. $d;
@@ -73,7 +75,8 @@ sub edge_points {
     return @edge;
 }
 
-# return true if the points represent a right triangle
+# return true if the points, together with the origin, represent a right
+# triangle
 sub is_rt {
     my @p = @{ $_[0] };
     my @q = @{ $_[1] };
@@ -81,12 +84,12 @@ sub is_rt {
     # is OP perpendicular to PQ?
     my $t1 = $p[0] * ($p[0] - $q[0]) + $p[1] * ($p[1] - $q[1]);
     #print "t1=$t1";
-    return 1 if abs($t1) < 1e-6;
+    return 1 if abs($t1) < 1e-9;
 
     # is OQ perpendicular to PQ?
     my $t2 = $q[0] * ($p[0] - $q[0]) + $q[1] * ($p[1] - $q[1]);
     #print "t2=$t2";
-    return 1 if abs($t2) < 1e-6;
+    return 1 if abs($t2) < 1e-9;
 
     return 0;
 }
